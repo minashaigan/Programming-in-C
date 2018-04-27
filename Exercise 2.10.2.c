f583e0e7427a7e7c0a45576f7fefeb7541389bea
Mina Shaigan <mina.shaigan@gmail.com>
2018-04-28 01:31:32 +0430

    Add files via upload

---
+++ Exercise 2.10.2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * Function:  main
+ * ------------------
+ *  prints name and family in each line
+ *
+ *  returns: Mina
+ *           Shaigan
+ */
+int main()
+{
+    printf("Mina\nShaigan");
+}
