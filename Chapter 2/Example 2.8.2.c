3149297d46358199a6f83b943cef360d010da401
Mina Shaigan <mina.shaigan@gmail.com>
2018-04-28 01:20:40 +0430

    Add files via upload

---
+++ Chapter 2/Example 2.8.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define STARS "******"
+
+/*
+ * Function:  main
+ * ------------------
+ *  has a symbolic constant named STARS
+ *
+ *  returns: ******
+ *           there are 6 stars above and 6 stars below
+ *           ******
+ */
+int main()
+{
+    printf(STARS);
+    printf("\nthere are 6 stars above and 6 stars below\n");
+    printf(STARS);
+}
