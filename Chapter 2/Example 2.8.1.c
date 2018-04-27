2b8a0405a14744cfd38e25048c2133092138dfef
Mina Shaigan <mina.shaigan@gmail.com>
2018-04-27 23:22:08 +0430

    Add files via upload

---
+++ Chapter 2/Example 2.8.1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * Function:  main
+ * ------------------
+ *  prints an H on the screen
+ *
+ *  returns: 0  0
+ *           0000
+ *           0  0
+ */
+int main()
+{
+    printf("0  0\n");
+    printf("0000\n");
+    printf("0  0\n");
+}
