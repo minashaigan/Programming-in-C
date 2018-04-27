2608615fc2a751e2eec253a2c4302de2cda89d8f
Mina Shaigan <mina.shaigan@gmail.com>
2018-04-28 01:25:26 +0430

    Add files via upload

---
+++ Chapter 2/Exercise 2.10.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * Function:  main
+ * ------------------
+ *  prints an expression
+ *
+ *  returns: I started learning C
+ */
+int main()
+{
+    printf("I started learning C");
+}
