3b1820956753c20cc9667761d1eee4f6e6bfc5d4
Mina Shaigan <mina.shaigan@gmail.com>
2018-04-27 23:11:55 +0430

    Add files via upload

---
+++ Chapter 2/Example 2.2.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * Function:  main
+ * ------------------
+ *  displays an expression
+ *
+ *  returns: my first program in C language.
+ */
+int main()
+{
+    printf("my first program in C language.");
+}
+
