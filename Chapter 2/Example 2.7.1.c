29530afffaa7f6c400f4fb66c437aa91e0078b3f
Mina Shaigan <mina.shaigan@gmail.com>
2018-04-27 23:18:56 +0430

    Add files via upload

---
+++ Chapter 2/Example 2.7.1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * Function:  main
+ * ------------------
+ *  displays several programming languages and their creators
+ *
+ *  returns: BCPL    Martin Richards
+ *           B       Ken Thompson
+ *           C       Denis Ritchie
+ *           C++     Bjarne Stroustrup
+ */
+int main()
+{
+    printf("BCPL\tMartin Richards\n");
+    printf("B\tKen Thompson\n");
+    printf("C\tDenis Ritchie\n");
+    printf("C++\tBjarne Stroustrup\n");
+}
