5e7a1deec60512270d6b7800828fa10a8ad4cd4e
Mina Shaigan <mina.shaigan@gmail.com>
2018-04-28 14:23:10 +0430

    Add files via upload

---
+++ Chapter 3/Example 3.1.2.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * Function:  main
+ * ------------------
+ * initialization
+ *
+ */
+int main()
+{
+    char string1[20];
+    char string2[20] = "computer science";
+    char string3[] = "computer science";
+}
+
