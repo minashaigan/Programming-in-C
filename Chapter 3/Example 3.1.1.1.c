e095756f7c90a93d5b741bd73807077d8a4f0a22
Mina Shaigan <mina.shaigan@gmail.com>
2018-04-28 14:10:25 +0430

    Add files via upload

---
+++ Chapter 3/Example 3.1.1.1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * Function:  main
+ * ------------------
+ *  declaration
+ *
+ *  returns:
+ */
+int main()
+{
+    int i, n;
+    float grade, sum, x;
+    double error;
+    char letter, c1, c2, line[20];
+}
