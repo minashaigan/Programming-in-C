2ace31a010eca7e1b457b42c6e7f9875c483cd04
Mina Shaigan <mina.shaigan@gmail.com>
2018-04-28 14:17:33 +0430

    Add files via upload

---
+++ Chapter 3/Example 3.1.1.2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * Function:  main
+ * ------------------
+ * computes the area and perimeter of a circle of radius 2.5
+ *
+ *  returns: The area is: 19.634375
+ *           Perimeter is: 15.707500
+ */
+int main()
+{
+    float radius, area, perimeter;
+    radius = 2.5;
+    area = 3.1415*radius*radius;
+    perimeter = 2*3.1415*radius;
+    printf("The area is: %f\n", area);
+    printf("Perimeter is: %f\n", perimeter);
+}
+
