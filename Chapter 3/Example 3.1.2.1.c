511c80de7822b72409b2a57d9557ac6de467c2b4
Mina Shaigan <mina.shaigan@gmail.com>
2018-04-28 14:20:05 +0430

    Add files via upload

---
+++ Chapter 3/Example 3.1.2.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * Function:  main
+ * ------------------
+ * initialization
+ *
+ */
+int main()
+{
+    int n = 100, i;
+    float sum = 0.0;
+    char c1, c2 = 'A', c3;
+}
+
