aabcfaf1e16e9e8978d49e2735dbb9a6db35372c
Mina Shaigan <mina.shaigan@gmail.com>
2018-04-30 14:21:25 +0430

    Add files via upload

---
+++ Chapter 4/Example 4.1.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Function:  main
+ * ------------------
+ * displays an expression
+ *
+ *  returns: a = 12 and b = -25
+ */
+int main()
+{
+    printf("a = %d and b = %d", 12, -5*5);
+}
