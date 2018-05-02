98c4201328b512ddb158cf1594e2a51c52ae52e0
Mina Shaigan <mina.shaigan@gmail.com>
2018-05-02 14:25:24 +0430

    Add files via upload

---
+++ Chapter 4/Exercise 4.6.1.3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * Function:  main
+ * ------------------
+ *  putchar() function
+ *
+ *  returns: *
+ */
+int main()
+{
+    putchar('+');
+	putchar('\b');
+	putchar('*');
+}
